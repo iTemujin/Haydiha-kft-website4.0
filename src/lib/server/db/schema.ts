c946cf74bd2b538049b877775abb2321347be112
temu <tumukhan@icloud.com>
2026-02-01 22:07:37 +0100

    login the email DONE

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,14 +1,15 @@
-import { pgTable, integer, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, integer, varchar, timestamp } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+export const users = pgTable('user', {
     email: varchar({ length: 50 }).unique().notNull(),
-    code: varchar({ length: 4 }).notNull(),
+    code: varchar({ length: 6 }).notNull(),
     role: varchar().default("customer"),
-    session: varchar().unique()
+    session: varchar({ length: 64 }).unique(),
+    sessionExpiresAt: timestamp(),
 });
 
-export const product = pgTable("product", {
-    vendor: varchar().references(() => user.email, { onDelete: 'cascade' }), // Elado vendor = kereskedo
+export const products = pgTable("product", {
+    vendor: varchar().references(() => users.email, { onDelete: 'cascade' }), // Elado vendor = kereskedo
     productName: varchar({ length: 50 }).notNull().unique(),
     category: varchar().array(), 
     price: integer().notNull(),
@@ -16,16 +17,16 @@ export const product = pgTable("product", {
     imgLink: varchar().notNull(),
 });
 
-export const productSize = pgTable("Size", {
+export const productSizes = pgTable("Size", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    productName: varchar().references(() => product.productName, { onDelete: 'cascade' }),
+    productName: varchar().references(() => products.productName, { onDelete: 'cascade' }),
     size: varchar({ length: 50 }).notNull(),
     pieces: integer().notNull(),
 });
 
-export const order = pgTable("order", {
+export const orders = pgTable("order", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    customer: varchar().references(() => user.email),
-    productId: integer().references(() => productSize.id, { onDelete: 'cascade' }),
+    customer: varchar().references(() => users.email),
+    productId: integer().references(() => productSizes.id, { onDelete: 'cascade' }),
     role: varchar().notNull()
 });
--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -1,19 +1,75 @@
 import { db } from '$lib/server/db'
-import { user } from '$lib/server/db/schema'
+import { users } from '$lib/server/db/schema'
 import { json } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
+import crypto from 'crypto'
+
+async function email(email_, cookies) {
+    const email = email_.toLowerCase();
+    const code = crypto.randomInt(10000, 100000);
+    let session;
+    while (true)
+    {
+        session = crypto.randomBytes(32).toString('hex');
+
+        const result = await db.select()
+        .from(users)
+        .where(eq(users.session, session))
+        if (result.length == 0) { break }
+    }
+
+    const result = await db.select()
+    .from(users)
+    .where(eq(users.email, email));
+
+    try {
+        if (result.length >= 1)
+        {
+            await db.update(users)
+            .set({
+                code: code,
+                session: session,
+                sessionExpiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 16),
+            })
+            .where(eq(users.email, email));
+        } else 
+        {
+            await db.insert(users)
+            .values({
+                email: email,
+                code: code,
+                session: session,
+                sessionExpiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 16),
+            });
+        }
+    } catch {
+        return false
+    }
+    console.log(code)
+    cookies.set('user', session, {
+    path: '/',
+    httpOnly: true,
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 16
+    });
+    return true
+}
+
+
 
 export async function POST({ request, cookies }) {
     const { message, me } = await request.json();
 
     if (message == 'email')
     {
-        const email = me.toLowerCase();
-        const result = await db.select().from(user).where(eq(user.email, email));
-        if (result.length >= 1)
-        {
-            return json({ email:'', error:'Mar van ilyen felhasznalo' }, { status: 201});
+        if (!await email(me, cookies)) {
+            return json({ email: 'No', error: 'Nem helyes a formátum',}, { status: 201 })
         }
+        
+    } else if ( message == 'code' )
+    {
+        
     }
-    return json({ email:me, error:'error' }, { status: 201});
+
+    return json({ email:me, error:'it hasnt error' }, { status: 201});
 }
